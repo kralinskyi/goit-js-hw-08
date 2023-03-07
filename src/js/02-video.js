2e53ffd9ec519fdd47cc9329aa3425b27dae0146
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-03-07 17:04:40 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const timeKey = 'videoplayer-current-time';
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(timeKey, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(timeKey));
