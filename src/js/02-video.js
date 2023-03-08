9a0daa3f735465b8688d690006559d3eb2a606a9
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-03-08 14:36:56 +0200

    new key

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,12 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-const timeKey = 'videoplayer-current-time';
+const TIME_KEY = 'videoplayer-current-time';
 
 const onPlay = function ({ seconds }) {
-  localStorage.setItem(timeKey, seconds);
+  localStorage.setItem(TIME_KEY, seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem(timeKey));
+player.setCurrentTime(localStorage.getItem(TIME_KEY));
